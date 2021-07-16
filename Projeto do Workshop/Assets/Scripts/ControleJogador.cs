4c06272c414847c254f6acce3a168765fe8a5fe6
Lucas Duez <lucas.verzaro@gmail.com>
2021-07-16 14:22:30 -0300

    Animacoes prontas
    Agora ao pular, ou andar, ou ficar parado, as animações são iniciadas

--- Projeto do Workshop/Assets/Scripts/ControleJogador.cs
+++ Projeto do Workshop/Assets/Scripts/ControleJogador.cs
@@ -13,9 +13,12 @@ public class ControleJogador : MonoBehaviour
     float forcaPulo;
 
     bool podePular;
+
+    Animator anim;
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        rig  = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -28,8 +31,17 @@ public class ControleJogador : MonoBehaviour
     private void andar(){
         float direcao = Input.GetAxisRaw("Horizontal");
         rotaciona(direcao);
+        transitaAnimacoes(1);
 
         rig.velocity = new Vector2(direcao * velocidade, rig.velocity.y);
+
+        if(direcao == 0 && podePular){ //Quando parado, Idle
+            transitaAnimacoes(0);
+        }
+
+        if(direcao != 0 && !podePular){ //Quando correndo e pulando, Pulo
+            transitaAnimacoes(2);
+        }
     }
     
     private void pular(){
@@ -37,12 +49,15 @@ public class ControleJogador : MonoBehaviour
         if(inputPulo && podePular){
             rig.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
             podePular = false;
+
+            transitaAnimacoes(2);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D outro) { //Verifica se ele está no chão. Se estiver, pode pular    
         if(outro.gameObject.CompareTag("Chao")){
             podePular = true;
+            transitaAnimacoes(0);
         }
     }
 
@@ -51,6 +66,10 @@ public class ControleJogador : MonoBehaviour
         if(direcao != 0){
             this.transform.localScale = new Vector3(direcao,1f,1f);
         }
+    }
+
+    private void transitaAnimacoes(int idAnimacao){
+        anim.SetInteger("Animacao", idAnimacao);
     }
 
 }
